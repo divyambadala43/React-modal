56e8a47f1c85e282b64fe7b949d7457b26165040
Divyam <divyambadala43@gmail.com>
2021-02-21 02:02:59 +0530

    add modal with css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,88 @@
 import React from 'react'
 import styled from 'styled-components'
+import modalImage from './modal.jpg'
+import {MdClose} from 'react-icons/md'
+
+const Background = styled.div`
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ModalWrapper = styled.div`
+    height: 500px;
+    width: 800px;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+    background: #fff;
+    color: #000;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    position: relative;
+    z-index: 10;
+    border-radius: 10px;
+
+`
+
+const ModalImg = styled.img`
+    width:100%;
+    height: 100%;
+    border-radius: 10px 0 0 10px;
+    background: #000;
+`
+
+const ModalContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    line-height: 1.8;
+    color: #141414;
+    p{
+        margin-bottom: 1rem;
+    }
+    button{
+        padding: 10px 24px;
+        background: #141414;
+        color: #fff;
+        border: none;
+    }
+`
+
+const CloseModalButton = styled(MdClose)`
+    cursor: pointer;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 32px;
+    height: 32px;
+    padding: 0;
+    z-index: 10;
+`
 
 export const Modal = ({showModal, setShowModal}) => {
     return (
-        <div>
-            {showModal ? <div>Modal</div> : null  }
-        </div>
+        <>
+            {showModal ? <Background>
+                <ModalWrapper showModal={showModal}>
+                    <ModalImg src={modalImage} alt='camera' />
+                    <ModalContent>
+                        <h1>Are you ready</h1>
+                        <p>Get exclusive access to our new launch.</p>
+                        <button>
+                            Join Now
+                        </button>
+                    </ModalContent>
+                    <CloseModalButton onClick={() => {
+                            setShowModal(prev => !prev)
+                        }} />
+                </ModalWrapper> 
+            </Background>: null}
+            
+        </>
     )
 }
 
