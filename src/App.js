5ca5d96565f6e378928154a99327c0f89e0e6fa3
Divyam <divyambadala43@gmail.com>
2021-02-21 00:53:40 +0530

    add modal button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background-color: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+
+`
 
 function App() {
   return (
     <>
-      <h1>What Up!!</h1>
+      <Container>
+        <Button>I am a Modal</Button>
+      </Container>
     </>
   );
 }
